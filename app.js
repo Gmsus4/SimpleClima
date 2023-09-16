32b64709e1482e0eb1d4b98f16756288e3f4203e
Fernando <jesusfgm45@gmail.com>
2023-09-16 09:31:48 -0600

    version 2.0

--- app.js
+++ app.js
@@ -3,29 +3,21 @@ const button = document.querySelector('.btn');
 const img = document.querySelector('.img');
 const imgCountry = document.querySelector('.country');
 const display = document.querySelector('.display');
-
 const displayData = document.querySelector('.res_data');
 const displayFooter = document.querySelector('.res_footer');
 const displayHeader = document.querySelector('.res_name');
-
 const spanTextClima = document.querySelector('.clima');
 const spanNameLocation = document.querySelector('.name');
 const spanTextHumedad = document.querySelector('.humedad');
 const spanTextViento = document.querySelector('.viento');
-
 const APIKEY = 'ccf99b6dca26359e28390bd47ee4a0c1';
 let value;
 let weatherData;
 
 async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${APIKEY}`;
-    
     const response = await fetch(url);
-
-    if (response.status === 404) {
-        throw new Error('La ciudad no fue encontrada.'); // Lanza un error personalizado
-    }
-
+    if (response.status === 404) {throw new Error('La ciudad no fue encontrada.'); }
     const data = await response.json();
     return data;
 }
@@ -33,19 +25,16 @@ async function getWeather() {
 async function setData() {
     try {
         const weatherData = await getWeather();
+        const country = weatherData.sys.country.toLowerCase();
         displayData.classList.remove('display');
         displayFooter.classList.remove('display');
         display.classList.remove('display');
         displayHeader.classList.remove('error');
         imgCountry.classList.remove('set_img_error');
-
-        const country = weatherData.sys.country.toLowerCase();
-
         spanTextClima.innerHTML = `${Math.round(weatherData.main.temp - 273.15)}°c`;
         spanNameLocation.innerHTML = weatherData.name;
         spanTextHumedad.innerHTML = `${weatherData.main.humidity} %`;
         spanTextViento.innerHTML = `${weatherData.wind.speed} m/s`;
-
         imgCountry.src = `https://flagcdn.com/16x12/${country}.png`;
 
         switch (weatherData.weather[0].main) {
